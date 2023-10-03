2d3f6c7ba58d615eeb1e8c56b70f1839f167e736
Emanuel Peixoto <epcri1966@gmail.com>
2023-10-02 22:30:32 -0300

    Add files via upload

---
+++ AULA FRONT/ex5.js/Quest2.js
@@ -0,0 +1,22 @@
+
+/* Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de
+números pares e a quantidade de números ímpares.
+*/
+
+
+let numerosPares = 0;
+let numerosImpares = 0;
+
+for (let i = 1; i <= 10; i++) {
+  let numero = parseInt(prompt(`Digite o ${i}º número:`));
+
+    if (numero % 2 === 0) {
+      numerosPares++;
+    } else {
+      numerosImpares++;
+    }
+  }
+
+
+console.log(`Quantidade de números pares: ${numerosPares}`);
+console.log(`Quantidade de números ímpares: ${numerosImpares}`);
